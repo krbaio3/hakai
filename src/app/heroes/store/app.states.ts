09c734e554af160fcf9a94cd410d18d721718798
KrBaIO3 <krbaio3@gmail.com>
2018-06-29 10:18:03 +0200

    /authenticationNGRX: Auto stash before merge of "feature/authenticationNGRX" and "origin/feature/authenticationNGRX"

---
+++ src/app/heroes/store/app.states.ts
@@ -0,0 +1,10 @@
+import { User } from '../models/user';
+
+export interface State {
+  // is a user authenticated?
+  isAuthenticated: boolean;
+  // if authenticated, there should be a user object
+  user: User | null;
+  // error message
+  errorMessage: string | null;
+}
